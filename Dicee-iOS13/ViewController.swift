d93da544db36154bf58e2b8bfb30643ac81378aa
Prasshant Shanmugalingam <stickoking89@icloud.com>
2023-06-11 17:42:18 +0800

    Implement Roll Functionality.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -2,19 +2,48 @@
 //  ViewController.swift
 //  Dicee-iOS13
 //
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
+//  Created by Prasshant Shanmugalingam on 11/06/2023.
+//
+
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Prasshant Shanmugalingam on 11/06/2023.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    let dice: [String] = ["DiceOne", "DiceTwo", "DiceThree", "DiceFive", "DiceSix"]
+    
+    
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        adjustOpacity(true)
+        
+      
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageViewOne.image = UIImage(imageLiteralResourceName: dice.randomElement()!)
+        diceImageViewTwo.image = UIImage(imageLiteralResourceName: dice.randomElement()!)
+        adjustOpacity(false)
+        
+    }
+    
+    func adjustOpacity(_ initialize: Bool) {
+        if (initialize) {
+            diceImageViewOne.alpha = 0.5
+            diceImageViewTwo.alpha = 0.5
+        }
+        else {
+            diceImageViewOne.alpha = 1
+            diceImageViewTwo.alpha = 1
+        }
+    }
+    
 }
-
